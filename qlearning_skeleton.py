2da604baabe268aa56f8ba43329802dd2dad1809
abrianas <54454184+abrianas@users.noreply.github.com>
2019-11-16 19:49:01 -0500

    filled in more functions

--- qlearning_skeleton.py
+++ qlearning_skeleton.py
@@ -9,23 +9,23 @@ import gym
 # an epsilon in the range [0, 1] and the state
 # to output actions according to an Epsilon-Greedy policy
 # (random actions are chosen with epsilon probability)
-def tabular_epsilon_greedy_policy(Q, eps, state, train=False): ###
-    # if train=true then no random actions selected
+def tabular_epsilon_greedy_policy(Q, eps, state, train_flag): ###
+    # if train_flag=True then no random actions are selected
     n_actions = Q.shape[1]
-    if train or np.random.rand() < eps:
+    if train_flag or np.random.rand() > eps: ## rename the train variable
         action = np.argmax(Q[state, :])
     else:
         action = np.random.randint(0, n_actions)
-        
     return action
 
 
 class QLearning(object):
     # Initialize a Qlearning object
     # alpha is the "learning_rate"
-    def __init__(self, num_states, num_actions, alpha=0.5, gamma=0.9):
+    def __init__(self, num_states, num_actions, alpha=0.5, gamma=0.8):
         # initialize Q values to something
-        self.Q = np.zeros((num_states, num_actions))
+        #self.Q = np.zeros((num_states, num_actions))
+        self.Q = np.random.random((num_states, num_actions))
         self.alpha = alpha
         self.gamma = gamma
 
@@ -36,14 +36,11 @@ class QLearning(object):
     # done is a bool indicating if the episode terminated at this step
     # you can return anything from this function to help with plotting/debugging
     def update(self, state, action, reward, next_state, done):
-        # a = np.argmax(self.Q[next_state, :]) # should I use np.argmax or np.max?
+        a = np.argmax(self.Q[next_state, :])
         if done: # at the terminal state Q[next_state,a]=0
             self.Q[state,action] += self.alpha*(reward-self.Q[state,action])
         else:
-            self.Q[state,action] += self.alpha*(reward+(self.gamma*np.max(self.Q[next_state, 
-                :]))-Q[state,action])
-            
-        return
+            self.Q[state,action] += self.alpha*(reward+(self.gamma*self.Q[next_state,a])-self.Q[state,action])
 
 
 
@@ -51,20 +48,66 @@ class QLearning(object):
 # run the greedy policy (no randomness) on the environment for niter number of times
 # and return the fraction of times that it reached the goal
 def evaluate_greedy_policy(qlearning, env, niter=100):
-    pass
+    # TODO: write training code here
+    tstep_reward = [] 
+    
+    for e in range(num_episodes):
+        state = env.reset()
+        total = 0
+        t = 0
+        done = False
+        while t<niter:
+            t += 1
+            action = tabular_epsilon_greedy_policy(qlearning.Q, eps, state, True) # we want randomness
+            next_state, reward, done = env.step(action)
+            total += reward
+            qlearning.update(state, action, reward, next_state, done)
+            state = next_state
+            if done:
+                tstep_reward.append(total)
+                break
+        
+    # must the number of times the goal is reached    
+    return tstep_reward
 
+
+# this function will take in an environment(GridWorld),
+# a Qlearning object,
+# an epsilon in the range [0, 1],
+# and the number of episodes you want to run the algorithm for
+# Returns ...
+def offTD(env, qlearning, num_episodes, eps):
+    # TODO: write training code here
+    tstep_reward = [] 
+    
+    for e in range(num_episodes):
+        state = env.reset()
+        total = 0
+        done = False
+        while True:
+            action = tabular_epsilon_greedy_policy(qlearning.Q, eps, state, False) # we want randomness
+            next_state, reward, done = env.step(action)
+            total += reward
+            qlearning.update(state, action, reward, next_state, done)
+            state = next_state
+            if done:
+                tstep_reward.append(total)
+                break
+        
+    # must print out iteration number for one of the questions    
+    return tstep_reward
 
 if __name__ == "__main__":
+    num_episodes = 1000
+    eps = 0.1
     env = GridWorld(MAP3)
     qlearning = QLearning(env.get_num_states(), env.get_num_actions())
-    # must create a function to train the code
-
-    # ## TODO: write training code here
-    # num_episodes = 100
-    # eps = 0.1
-    # for i in range(num_episodes):
-        # pass
-
+    tstep_reward = offTD(env, qlearning, num_episodes, eps)
+    plt.plot(tstep_reward)
+    plt.xlabel("Number of Episodes")
+    plt.ylabel("Total Rewards")
+    plt.title("eps-Greedy w/ Randomness")
+    plt.show()
 
     # # evaluate the greedy policy to see how well it performs
     # frac = evaluate_greedy_policy(qlearning, env)
